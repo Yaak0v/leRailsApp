33aeef61089b83c99359e7b781a0dfe827c9a717
Yaak0v <jacobsandry@gmail.com>
2021-09-19 23:01:24 -0400

    activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token  
     before_save { self.email = email.downcase }
+    #the above can also be done like the before_create callback, with a private method defined below
+    before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
@@ -11,5 +14,15 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+end
+
+def User.new_token
+SecureRandom.urlsafe_base64
+end
+
+private 
+def create_activation_digest
+  self.activation_token = User.new_token
+  self.activation_digest = User.digest(activation_token)
 end
 end
