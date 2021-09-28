387f3d6d4c6246c690bfcc7e42762146584aa993
Yaak0v <jacobsandry@gmail.com>
2021-09-27 20:30:54 -0400

    micropost form

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,6 +63,10 @@ end
     reset_sent_at < 2.hours.ago
   end
 
+  def feed  
+    Micropost.where("user_id = ?", id)
+  end
+
 private 
 def create_activation_digest
   self.activation_token = User.new_token
